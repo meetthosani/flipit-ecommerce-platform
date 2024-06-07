9b10eb15b46cc8b792ec25e9898aabad2f3279c8
meetthosani <meet.thosani@gmail.com>
2024-06-07 16:45:31 -0400

    rendered latest products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main className="py-3">
         <Container>
           <h1>Welcome to Flipit!</h1>
+          <Home />
         </Container>
       </main>
       <Footer />
